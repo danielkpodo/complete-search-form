d47038a85f30dff38f23368ab312043a2342e55a
naphtha <naphthanewman@gmail.com>
2019-07-18 07:55:28 +0000

    pulled dummy article content from database

--- index.php
+++ index.php
@@ -6,9 +6,38 @@
         <input type="text" name="search" placeholder="Search form...">
         <button type="submit" name="search-form">Search</button>
     </form>
+    <h1>Article Front page</h1>
+    <h2>All article</h2>
+    <div class="article-container">
+        <?php
+        $article_query = "SELECT * FROM article";
+        $article_result = mysqli_query($conn, $article_query);
+        if (mysqli_num_rows($article_result) > 0) {
+            while ($row = mysqli_fetch_assoc($article_result)) {
+                $article_id = $row['article_id'];
+                $article_title = $row['article_title'];
+                $article_content = $row['article_content'];
+                $article_date = $row['article_date'];
+                $article_author = $row['article_author'];
+                ?>
+        <div class="article">
+            <h3><?php echo $article_title; ?></h3>
+            <p><?php echo $article_content; ?></p>
+            <p><?php echo $article_date; ?></p>
+            <p><?php echo $article_author; ?></p>
+        </div>
 
+
+        <?php
+            }
+        } else {
+            echo "No articles yet!";
+        }
+        ?>
+
+    </div>
 
 
 </body>
 
-<?php include_once("./footer.php");
+<?php include_once("./footer.php"); ?>
