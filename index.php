a12459ae06ceab435be2c4101253b38c1ca64340
naphtha <naphthanewman@gmail.com>
2019-07-18 07:25:25 +0000

    created html form field for search

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PHP Search form</title>
-</head>
+<?php include_once("./header.php"); ?>
 
 <body>
 
+    <form action="search.php" method="post">
+        <input type="text" name="search" placeholder="Search form...">
+        <button type="submit" name="search-form">Search</button>
+    </form>
+
+
+
 </body>
 
-</html>
+<?php include_once("./footer.php");
